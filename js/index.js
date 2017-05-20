888dcfef179156863be9fa7db0b7b5a85c62f41f
jeanpaul1304 <jeanpaul1304>
2017-05-20 16:04:19 -0500

    add first cuadro

--- js/index.js
+++ js/index.js
@@ -7,13 +7,64 @@ $('#bankClientAccount').selectpicker('refresh');
 $('#bankAylluAccount').selectpicker('refresh');
 $('#bankToPay').selectpicker('refresh');
 
+changeAllToPen()
+
 function bindCalculator(){
     $('.js-btn-radios').on('click',function(){
         let $this = $(this)
         cleanAll()
-        console.log($this.val())
+        if($this.data('currency') == 'PEN'){
+            changeAllToPen()
+        }else{
+            changeAlltoDollar()
+        }
+    })
+    $('#bankToPay').on('change',function(){
+        let $this = $(this)
+        let valor = $this.find('option:selected').attr('data-value')
+        $('.js-aylluaccount').text(valor)
+        $('.js-aylluaccountCCI').text('234-'+valor+'23')
     })
 }
+
+function changeAllToPen(){
+    $('.js-platformCurrecy').text('$')
+    $('.js-myCurrency').text('S/')
+    $('#myMoney').unbind('keyup')
+    $('#myMoney').keyup(calculatePen)
+}
+function changeAlltoDollar(){
+    $('.js-platformCurrecy').text('S/')
+    $('.js-myCurrency').text('$')
+    $('#myMoney').unbind('keyup')
+    $('#myMoney').keyup(calculateDollar)
+}
+
+function calculatePen(){
+    let tc = 3.25
+    let amount = $('.js-myMoneyFirst').val()
+    let newAmount = (amount/3.25).toFixed(3)
+    let comision = (newAmount*0.01).toFixed(2)
+    $('.js-finalChange').text(' $'+newAmount)
+    $('.js-finalComision').text(' $'+comision)
+    $('.js-finalCash').text(' $'+(newAmount-comision).toFixed(3))
+    $('.js-myMoney').val(amount)
+    $('.js-txt-changeAmount').val(newAmount-comision)
+    $('.js-initialCash').text(' S/'+(amount))
+}
+function calculateDollar(){
+    let tc = 3.25
+    let amount = $('.js-myMoneyFirst').val()
+    let newAmount = (amount*3.25).toFixed(3)
+    let comision = (amount*0.01).toFixed(2)
+    $('.js-finalChange').text(' S/'+newAmount)
+    $('.js-finalComision').text(' S/'+comision)
+    $('.js-finalCash').text(' S/'+(newAmount-comision).toFixed(3))
+    $('.js-myMoney').val(amount)
+    $('.js-txt-changeAmount').val(newAmount-comision)
+    $('.js-initialCash').text(' $'+(amount))
+}
+
 function cleanAll(){
     $('input[type="text"]').val('')
 }
