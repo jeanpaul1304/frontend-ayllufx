8a82fdbe7aef6afb17d80e3a390e4987a8362fcd
josedelcastillo <j.delcastillos@gmail.com>
2017-05-20 12:44:26 -0500

    integración OK

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-function cargarTablaMarcacionesAyer(){
+function crearSolicitud(){
 	var user = $("#user").val();
 	var exchangeRate=$("#exchangeRate").val();
 	var clientAccount=$("#clientAccount").val();
@@ -13,8 +13,18 @@ function cargarTablaMarcacionesAyer(){
 	var operationNumber=$("#operationNumber").val();
 	
 	var mail=$("#mail").val();
-	
-	var data ={
+	var data="?idCliente="+user+
+	"&tipoCambio="+exchangeRate+
+	"&cuentaBancoDestino="+clientAccount+
+	"&bancoDestino="+bankClientAccount+
+	"&montoEntregado="+amount+
+	"&bancoOperacion="+bankAylluAccount+
+	"&numeroOperacion="+operationNumber+
+	"&monedaOrigen="+toCurrencyValue+
+	"&monedaDestino="+(toCurrencyValue=="PEN"?"PEN":"USD")+
+	"&mail="+mail;
+
+	var data1 ={
 		idCliente:user,
  tipoCambio:exchangeRate,
  cuentaBancoDestino:clientAccount,
@@ -35,18 +45,18 @@ entity: ""Solicitud Registrada, se atenderá en 2 días máximo""
 
 	 $.ajax({
 		type: "POST",
-		datatype: 'jsonp',
-		contentType: "application/json; charset=utf -8",
-		url: "http://192.168.65.25:8080/Ayllufx_rest/", 
-		data: data,
+		//datatype: 'json',
+		//contentType: false,
+		url: "http://192.168.65.25:8080/Ayllufx_rest/restDummy/solicitud/registro?"+data, 
+		
 		success: function(data) {
-				var rpta=JSON.parse(data);
-				if(rpta.statusCode==0)
-					$("#msgUser").html(rpta.entity);
+				
+				if(data.errorCode==0)
+					$("#msgUser").html(data.entity);
 			},
 	  	error: function (error) {
 	  				  alert("Ocurrio un error");
-	                  return -2;
+	                  
 	              }
 		});
 }
